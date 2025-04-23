73f64b30111820ac563ce86aa1eaa3dcc3df1a3b
kingrayChaska <oluwaseunrayomd082@gmail.com>
2025-04-23 12:29:52 +0100

    `Update package.json and data.js with new version numbers and links`

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export const projects = [
     description:
       "This web application lets users explore countries by selecting one to view detailed information. I designed the UI on my own and the application is fully responsive.",
     image: "./countriesInfo.gif",
-    link: "https://countries-info-app-regan.vercel.app/",
+    link: "https://countries-info-app-Chaska.vercel.app/",
   },
   {
     title: "Orion Psykoterapigruppen Website",
@@ -23,22 +23,22 @@ export const projects = [
     image: "./orion.gif",
     link: "https://orion-psykoterapigruppen.netlify.app/",
   },
-  // {
-  //   title: "NFT Shop",
-  //   subtitle: "Next.js and SASS",
-  //   description:
-  //     "I did many pair programming with my teammates. I learned how to use Next.js, SASS and some libraries, and how to implement design system into code.",
-  //   image: "./nftShop.gif",
-  //   link: "https://monakinft.vercel.app/",
-  // },
-  // {
-  //   title: "Game App",
-  //   subtitle: "JavaScript, HTML & CSS",
-  //   description:
-  //     "My communication skills had the chance to get sharpened since this game app contains many sub-games and the team had to make sure that all the games are working well. ",
-  //   image: "./gameApp.gif",
-  //   link: "https://cats-of-dogstreet-game.netlify.app/",
-  // },
+  {
+    title: "NFT Shop",
+    subtitle: "Next.js and SASS",
+    description:
+      "I did many pair programming with my teammates. I learned how to use Next.js, SASS and some libraries, and how to implement design system into code.",
+    image: "./nftShop.gif",
+    link: "https://monakinft.vercel.app/",
+  },
+  {
+    title: "Game App",
+    subtitle: "JavaScript, HTML & CSS",
+    description:
+      "My communication skills had the chance to get sharpened since this game app contains many sub-games and the team had to make sure that all the games are working well. ",
+    image: "./gameApp.gif",
+    link: "https://cats-of-dogstreet-game.netlify.app/",
+  },
   {
     title: "90s style webpage",
     subtitle: "HTML & CSS",
@@ -52,17 +52,31 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "...It is in making the course accessible to these wide ranging levels of knowledge where Regan demonstrates her deep understanding of coding and her empathy for students; she is able to analyze complex issues and reinterpret them in novel and compelling ways. Regan has an incredible capacity to distill a problem to its core contingent parts and work methodically outwards. She is a keen coder and a kind person.",
+      "...Working with Chaska was an absolute pleasure. His professionalism, technical expertise, and collaborative approach made our joint project a resounding success. He consistently delivered high-quality work on time, and his excellent communication skill ensured seamless integration. I highly recommend him to any web development team",
     image: "./isaac.jpeg",
     name: "Jayeoba Joseph",
     company: "Web Developer",
   },
   {
     quote:
-      "…Regan was reliable and showed up to do their assigned task, and assisted all people who came with curious questions about Hyper Island. …Regan was also well prepared for the task and showed great engagement throughout the event. …The Admissions team at Hyper Island are very pleased to have worked with Regan and would ask for their continued support in the future.",
+      "…Chaska was reliable and showed up to do their assigned task, and assisted all people who came with curious questions about Hyper Island. …Chaska was also well prepared for the task and showed great engagement throughout the event. …The Admissions team at Hyper Island are very pleased to have worked with Chaska and would ask for their continued support in the future.",
     image: "./angelica.jpeg",
     name: "Aderibigbe Inioluwa",
     company: "Amaezing Studio",
+  },
+  {
+    quote:
+      "…Chaska was reliable and showed up to do their assigned task, and assisted all people who came with curious questions about Hyper Island. …Chaska was also well prepared for the task and showed great engagement throughout the event. …The Admissions team at Hyper Island are very pleased to have worked with Chaska and would ask for their continued support in the future.",
+    image: "./angelica.jpeg",
+    name: "Ajala Daniel",
+    company: "UI/UX Designer",
+  },
+  {
+    quote:
+      "...Working with Chaska was an absolute pleasure. His professionalism, technical expertise, and collaborative approach made our joint project a resounding success. He consistently delivered high-quality work on time, and his excellent communication skill ensured seamless integration. I highly recommend him to any web development team",
+    image: "./isaac.jpeg",
+    name: "Jayeoba Joseph",
+    company: "Web Developer",
   },
 ];
 
